b073baf722c634e84f6437f6e4cb473e073740fe
Emanuelskod <Backhax@hotmail.com>
2023-02-16 13:07:39 +0100

    HTML AND CSS UPDATES

--- index.php
+++ index.php
@@ -5,8 +5,23 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+  <div class="container">
+
+  <div class="header">  
+    <h1 class="headerTxt">PARK IN PRAGUE</h1>
+</div>
+ 
+<div class="main">
+  <a href="" class="btn">PARK VEHICLE</a>
+  <a href="" class="btn">REMOVE VEHICLE</a>
+  <a href="" class="btn">SEARCH VEHICLE</a>
+  <a href="" class="btn">VIEW ALL VEHICLES</a>
+</div>
+  </div>
+
   
 </body>
 </html>
