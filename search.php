9f7f2c1218ab2be3a6ed0a27429c513ae2b8e3f8
Fredrik Höög <hoogfredrik84@gmail.com>
2023-02-16 16:07:48 +0100

    search update1

---
+++ search.php
@@ -0,0 +1,46 @@
+<?php
+include_once "classes/Connection.php";
+$conn = Connection::connection();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div class="container">
+  <div class="header">  
+    <h1 class="headerTxt">PARK IN PRAGUE</h1>
+</div>
+ 
+<div class="main">
+<a href="index.php" class="btn">HOME</a>
+  <a href="park.php" class="btn">PARK VEHICLE</a>
+  <a href="remove.php" class="btn">REMOVE VEHICLE</a>  
+  <a href="view.php" class="btn">VIEW ALL VEHICLES</a>
+  <form method="post">
+  <label for="RegNum">Insert registration number:</label><br>
+  <input type="text" name="RegNum" ><br><br>
+  <input type="submit" value="Submit"></form>
+</div>
+</div>  
+</body>
+</html>
+
+<?php
+$submit = $_POST['RegNum'];
+$sql = "SELECT RegNum FROM vehicles WHERE RegNum = $submit";
+$result = $conn->query($sql);
+
+foreach ($result as $key) {
+  echo $key['RegNum'];
+}
+
+
+$conn->close();
+?>
