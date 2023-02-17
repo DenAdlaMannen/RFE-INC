f8d551b8305563fe40fec6abe81214a834a44120
Emanuelskod <Backhax@hotmail.com>
2023-02-17 10:36:33 +0100

    FORM UPDATE, USE

--- park.php
+++ park.php
@@ -20,10 +20,27 @@ include_once "classes/park-class.php";
 </div>
  
 <div class="main">
+  <div class="menu">
   <a href="index.php" class="btn">HOME</a>
   <a href="remove.php" class="btn">REMOVE VEHICLE</a>
   <a href="search.php" class="btn">SEARCH VEHICLE</a>
   <a href="view.php" class="btn">VIEW ALL VEHICLES</a>
+  </div>
+
+  <div class="formContainer">
+    <h3 class="formHeader">Enter vehicle information</h3>
+    <div class="form">
+    <form method="POST" action="classes/park-class.php">
+      <label>Registration number: </label><input type="Text" name="regNum" placeholder="Registration Number">
+      <br>
+      <label>Type:</label>
+      <input type="Radio" name="type" value="MC"> MC
+      <input type="Radio" name="type" value="CAR"> CAR
+    </form>
+    </div>
+
+
+  </div>
 </div>
 
 <?php
