1d0961fd524247aa13bcb635a3e3573a5df9e5a6
Fredrik Höög <hoogfredrik84@gmail.com>
2023-02-18 20:43:35 +0100

    Update search-class.php

--- classes/search-class.php
+++ classes/search-class.php
@@ -8,14 +8,19 @@ function searchvehicles(){
 $functionconn = Connection::connection();
 //SETTING THE SEARCH UP
 $submit = $_POST['RegNum'];
+// IF EMPTY RETURN FAIL!
+if (empty($result)){
+    echo "That is not a valid registration number";
+}
+else{
 //QUERY FOR SEARCHING THE DB
 $sql = "SELECT RegNum, parkingspotid, arrivaltime FROM vehicles WHERE RegNum='$submit'";
 $result = $functionconn->query($sql);
-
 $row = $result->fetch_assoc();
 //CLOSING CONNECTION
 $functionconn->close();
     echo "The vehicle with registration number " . $row['RegNum'] . "<br> is parked at parkingspot " .$row['parkingspotid'] . "<br> and arrived at " . $row['arrivaltime'];  
-return $row; //vägrar returnera nåt de jävla aset!
+return $row; 
+}
 }
 ?>
